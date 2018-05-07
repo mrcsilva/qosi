4af6fb11e8556974cd94fd23d4623783ed066fd8
Mário Silva <mario156silva@gmail.com>
2018-05-07 22:56:17 +0100

    Persistent data

--- tp3/qosispeed/app/src/main/java/com/qosi/qosispeed/HomeFragment.java
+++ tp3/qosispeed/app/src/main/java/com/qosi/qosispeed/HomeFragment.java
@@ -1,6 +1,6 @@
 package com.qosi.qosispeed;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,27 +9,85 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
     View myView;
+    private SettingsFragment sf = null;
+    private TextView tv;
+    private Button start;
+    private boolean one;
+    private String text = "";
 
+
+    public void setSettings(SettingsFragment sf) {
+        this.sf = sf;
+    }
+
+    public void doOptions() {
+        View settingsView = sf.getView();
+        if(settingsView != null) {
+            Switch band = (Switch) settingsView.findViewById(R.id.bandwidth_switch);
+            Switch ping = (Switch) settingsView.findViewById(R.id.ping_switch);
+            Switch jitter = (Switch) settingsView.findViewById(R.id.jitter_switch);
+            String options = "";
+            if (band.isChecked()) {
+                System.out.println("Band!");
+                options += "- Bandwidth\n";
+                one = true;
+            }
+            else {
+                one = false;
+            }
+            if (ping.isChecked()) {
+                System.out.println("Delay!");
+                options += "- Delay (Ping)\n";
+                one = true;
+            }
+            else if(!one){
+                one = false;
+            }
+            if (jitter.isChecked()) {
+                System.out.println("Jit!");
+                options += "- Jitter\n";
+                one = true;
+            }
+            else if (!one){
+                one = false;
+            }
+
+            if (!one) {
+                System.out.println("Defined!");
+                text = "Go to Settings and choose what to test!";
+            } else {
+                System.out.println("Changed!\n" + options);
+                text = "Tests to perform:\n" + options;
+            }
+        }
+        else if(!one){
+            System.out.println("Default!");
+            tv.setText("Go to Settings and choose what to test!");
+            start.setEnabled(false);
+            start.setBackgroundColor(Color.parseColor("grey"));
+        }
+        else {
+            tv.setText(text);
+            start.setEnabled(true);
+            start.setBackgroundColor(Color.parseColor("#008cff"));
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.home_layout, container, false);
+        tv = (TextView) myView.findViewById(R.id.enabledOptions);
+        start = (Button) myView.findViewById(R.id.startButton);
 
-
-        TextView tv = (TextView) myView.findViewById(R.id.enabledOptions);
-        Button start = (Button) myView.findViewById(R.id.startButton);
-
-        if(tv.length() == 0) {
-            start.setEnabled(false);
-            start.setBackgroundColor(Color.parseColor("grey"));
-            tv.setText("Go to Settings and choose what to test!");
-        }
+        System.out.println("Passou aqui!");
+        doOptions();
 
         return myView;
     }
--- tp3/qosispeed/app/src/main/java/com/qosi/qosispeed/MainActivity.java
+++ tp3/qosispeed/app/src/main/java/com/qosi/qosispeed/MainActivity.java
@@ -2,8 +2,7 @@ package com.qosi.qosispeed;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.app.FragmentManager;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,8 +11,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SettingsFragment sf = new SettingsFragment();
+    private HomeFragment hf = new HomeFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +36,10 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_main);
         navigationView.setNavigationItemSelectedListener(this);
 
-        FragmentManager fm = getFragmentManager();
-        fm.beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
+        hf.setSettings(sf);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame, hf).commit();
 
 
     }
@@ -63,7 +69,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.nav_manage) {
+            return true;
+        }
+        else if(id == R.id.nav_home) {
             return true;
         }
 
@@ -75,17 +84,17 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
 
         if (id == R.id.nav_manage) {
-            // fragmentClass = SettingsFragment.class;
-            fm.beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+            fm.beginTransaction().replace(R.id.content_frame, sf).commit();
             setTitle(item.getTitle());
         }
         else if(id == R.id.nav_home) {
-            // fragmentClass = HomeFragment.class;
-            fm.beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
+            System.out.println("Carreguei no botao, entrou?");
+            fm.beginTransaction().replace(R.id.content_frame, hf).commit();
             setTitle("QoSI Speedtest");
+            hf.doOptions();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
